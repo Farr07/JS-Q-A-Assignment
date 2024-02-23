ecb0a6cfda5aba38f6a526e49d5d85e838d0893b
Farr07 <farr07ukh@gmail.com>
2024-02-23 05:51:27 -0800

    JS Assignment Questions solved

---
+++ JS QUESTIONS/script.js
@@ -0,0 +1,95 @@
+                                    // QUESTION NO 01
+
+ // Q1/- GET USER TO INPUT A NUMBER USING PROMPT () CHECK IF THE NUMBER IS A MULTIPLE OF 6 OR NOT ?
+ 
+
+// let num = +prompt("enter your number");
+// if(num % 6 === 0){
+//     document.write("is a multiple of 6")
+// }
+// else {
+//     document.write("is not  a multiple of 6") 
+// }
+
+
+
+                                    // QUESTION NO 02
+
+ // Q2/- WRITE A CODE WHICH CAN GIVE GRADE TO STUDENT ACCORDIND TO THEIR SCORES ?
+
+ 
+//  let score = +prompt("enter your percentage")
+//  let grade;
+  
+//  if(score >=90 && score<=100){
+//     document.write("A1")
+//  }
+//  else if(score >=80 && score<=89){
+//     document.write("A+")
+//  } else if(score >=70 && score<=79){
+//     document.write("A")
+//  }else if(score >=60 && score<=69){
+//     document.write("B")
+//  }
+//  else if(score >=50 && score<=59){
+//     document.write("C")
+//  }else if(score >=33 && score<=49){
+//     document.write("D")
+//  }
+//  else if(score >=0 && score<=33){
+//     document.write("FAIL")
+//  }
+
+
+
+                                   //  QUESTION NO 03
+
+// Q3/- CREATE MARKSHEET USING IF ELSE AND ELSE IF CONDITIONS ?
+
+
+// var name = prompt("name")
+// var english = +prompt("english")
+// var urdu = +prompt("urdu")
+// var physics = +prompt("physics")
+// var biology = +prompt("biology")
+// var chemistry = +prompt("chemistry")
+// var total = english + urdu + chemistry + physics + biology;
+// var percentage = (total / 500) *100;
+
+// if(percentage =>90 && percentage <= 100){
+// document.write(name +" your grade is A1" +" " + percentage +"%");
+// }
+// else if(percentage =>80 && percentage <= 89){
+// document.write(name +" your grade is A+" +" " + percentage +"%");
+   
+
+// }
+// else if(percentage =>70 && percentage <= 79){
+//     document.write(name +" your grade is A" +" "+ percentage +"%");
+
+
+// }
+// else if(percentage =>60 && percentage <= 69){
+// document.write(name +" your grade is B" +" "+ percentage +"%");
+   
+
+// }
+// else if(percentage =>50 && percentage <= 59){
+// document.write(name +" your grade is C" +" "+ percentage +"%");
+    
+
+// }
+// else if(percentage =>33 && percentage <= 49){
+// document.write(name +" your grade is D" +" "+ percentage +"%");
+ 
+
+// }
+// else if(percentage =>0 && percentage <= 32){
+//     document.write(name +" your grade is FAIL"+" " + percentage +"%");
+
+
+// }
+// else{
+//     document.write("enter invalid marks")
+// }
+
